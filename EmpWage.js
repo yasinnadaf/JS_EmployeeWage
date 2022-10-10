17a09020bbf4675ec6b398eafc57cce85fa0d6c6
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-10-10 23:45:11 +0530

    js employeeWage

---
+++ EmpWage.js
@@ -0,0 +1,10 @@
+//uc1
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+
+ if (empCheck == IS_ABSENT) {
+       console.log("uc1 - Employee is ABSENT");
+      // return;
+  } else {
+      console.log("uc1 - Employee is PRESENT")
+   }
